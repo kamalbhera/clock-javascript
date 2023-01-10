0a990fe637549c470fe1bda3463bd9575d2112e8
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-10 21:28:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    x = new Date();
+    htime = x.getHours();
+    mtime = x.getMinutes();
+    stime = x.getSeconds();
+    hrot = 30 * htime + mtime / 2;
+    mrot = 6 * mtime;
+    srot = 6 * stime;
+    hr.style.transform = `rotate(${hrot}deg)`;
+    min.style.transform = `rotate(${mrot}deg)`;
+    sec.style.transform = `rotate(${srot}deg)`;
+}, 1000);
